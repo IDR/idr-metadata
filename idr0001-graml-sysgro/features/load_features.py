eb9bbb6fea732a2b6a0fbbd4f907fcd5363c02cd
Simon Li <spli@dundee.ac.uk>
2015-06-22 21:22:01 +0100

    Rewrote SPW handler to use raw queries instead of BlitzGateway

--- idr0001-graml-sysgro/features/load_features.py
+++ idr0001-graml-sysgro/features/load_features.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import getpass
 import pandas
 import numpy as np
 import omero
 import omero.clients
 import omero.gateway
-from omero.rtypes import rstring
+from omero.rtypes import rstring, unwrap
 import re
 import yaml
 
@@ -42,23 +43,62 @@ class ProgressRecord(object):
 
 class SPW(object):
 
-    def __init__(self, client, screenid):
-        self.conn = omero.gateway.BlitzGateway(client_obj=client)
-        self.screen = self.conn.getObject('Screen', screenid)
-        assert self.screen
-
+    def __init__(self, session, screenid):
+        self.qs = session.getQueryService()
+        self.screenid = screenid
         self.plates = {}
-        for plate in self.screen.listChildren():
-            wells = {}
-            for well in plate.listChildren():
-                im = well.getImage()
-                if im:
-                    wells[(well.row, well.column, well.index)] = im
+        self.fetch(screenid)
 
-            try:
-                self.plates[plate.name].append(wells)
-            except KeyError:
-                self.plates[plate.name] = [wells]
+    def fetch(self, screenid):
+        for plateid, platename in self.get_plates(screenid):
+            self.plates[platename] = {'id': plateid}
+            acquisitions = {}
+            for acqid, acqname in self.get_plate_acquisitions(plateid):
+                acq = {'id': acqid}
+                wells = {}
+                for wid, wrow, wcol, iid, iname in \
+                        self.get_wellsamples_and_images(acqid):
+                    wells[(wrow, wcol)] = {
+                        'wid': wid, 'iid': iid, 'iname': iname}
+                acq['wells'] = wells
+                acquisitions[acqname] = acq
+            self.plates[platename]['acquisitions'] = acquisitions
+
+    def projection(self, q, id, nowrap=True):
+        params = omero.sys.ParametersI()
+        params.addId(id)
+        rs = self.qs.projection(q, params)
+        if nowrap:
+            return unwrap(rs)
+        return rs
+
+    def get_plates(self, screenid):
+        rs = self.projection(
+            'SELECT spl.child.id, spl.child.name FROM ScreenPlateLink spl '
+            'WHERE spl.parent.id=:id', screenid)
+        return sorted(rs)
+
+    def get_plate_acquisitions(self, plateid):
+        rs = self.projection(
+            'SELECT p.id, p.name FROM PlateAcquisition p WHERE p.plate.id=:id',
+            plateid)
+        return sorted(rs)
+
+    def get_wellsamples_and_images(self, acquisitionid):
+        rs = self.projection(
+            'SELECT ws.well.id, ws.well.row, ws.well.column, '
+            'ws.image.id, ws.image.name FROM WellSample ws '
+            'WHERE ws.plateAcquisition.id=:id', acquisitionid)
+        return sorted(rs)
+
+    def print_all(self):
+        for pk, pv in sorted(self.plates.iteritems()):
+            print '[Plate %d] %s' % (pv['id'], pk)
+            for ak, av in sorted(pv['acquisitions'].iteritems()):
+                print '  [PlateAcquisition %d] %s' % (av['id'], ak)
+                for wk, wv in sorted(av['wells'].iteritems()):
+                    print '    [Well %d %02d,%02d] [Image %d] %s' % (
+                        wv['wid'], wk[0], wk[1], wv['iid'], wv['iname'])
 
     def coord2offset(self, coord):
         ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -71,10 +111,11 @@ class SPW(object):
         c = int(ca)
         return r - 1, c - 1
 
-    def get_image(self, platename, runoffset, coord):
+    def get_image(self, platename, acqname, coord):
         r, c = self.coord2offset(coord)
-        im = self.plates[platename][runoffset][(r, c, 0)]
-        return im
+        wellim = self.plates[platename]['acquisitions'][acqname]['wells'][
+            (r, c)]
+        return wellim['iid']
 
 
 def connect(h, u, p, **kwargs):
@@ -86,6 +127,8 @@ def connect(h, u, p, **kwargs):
         if 'port' in proxy:
             props['Ice.SOCKSProxyPort'] = proxy['port']
     client = omero.client(h, props)
+    if not p:
+        p = getpass.getpass('\nEnter password for user %s: ' % u)
     session = client.createSession(u, p)
     return client, session
 
@@ -147,31 +190,52 @@ def create_feature(ms, vs):
     print 'create_feature %s %s...' % (ms.values, vs[:3].values)
 
 
+def get_dataframe(cfg, nrows=None):
+    dfmeta, dfvals, dfroi = load_features(
+        cfg['features'], cfg['metadatacolumns'], cfg['roicolumn'], nrows=nrows)
+    print dfmeta.dtypes.to_string()
+    print dfvals.dtypes.to_string()
+    print dfroi.dtypes
+    print cfg
+
+    assert dfroi is not None
+    dfmeta.insert(0, 'RoiID', np.int64(0))
+    print dfmeta.dtypes.to_string()
+
+    return dfmeta, dfvals, dfroi
+
+
+def select(dfmeta, platename, acqname):
+    cond = [dfmeta.experimentName == platename, dfmeta.plateName == acqname]
+    return np.where(np.all(cond, axis=0))[0]
+
+
 #####
 
-
 cfg = config('input.yml')
-dfmeta, dfvals, dfroi = load_features(
-    cfg['features'], cfg['metadatacolumns'], cfg['roicolumn'], nrows=1000)
-print dfmeta.dtypes.to_string()
-print dfvals.dtypes.to_string()
-print dfroi.dtypes
-print cfg
-
-assert dfroi is not None
+dfmeta, dfvals, dfroi = get_dataframe(cfg, 1000)
 scfg = cfg['server']
 proxy = scfg.get('socksproxy', {})
 client, session = connect(
     scfg['host'], scfg['user'], scfg['password'], proxy=proxy)
 
-dfmeta.insert(0, 'RoiID', np.int64(0))
-print dfmeta.dtypes.to_string()
+#####
+
+screenimages = SPW(session, cfg['screenid'])
+
+platename = 'X_110222_S1'
+acqname = 'Meas_01'
+indices = select(dfmeta, platename, acqname)
 
 p = ProgressRecord(cfg['progresslist'])
-for i in xrange(dfmeta.shape[0]):
+for i in indices:
     print 'Row', i
     if 'r' not in p.get(i):
-        roi = create_roi(dfroi.iloc[i], -1)
+        meta = dfmeta.iloc[i]
+        roi = dfroi.iloc[i]
+        vals = dfvals.iloc[i]
+        iid = screenimages.get_image(platename, acqname, meta.well)
+        roi = create_roi(dfroi.iloc[i], iid)
         p.set(i, 'r')
     if 'f' not in p.get(i):
         ft = create_feature(dfmeta.iloc[i], dfvals.iloc[i])
