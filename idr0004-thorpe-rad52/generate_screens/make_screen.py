a77fad5e94cc807dc082c4dbbbee47dafcc86498
simleo <simleo@crs4.it>
2015-10-01 15:56:59 +0100

    rad52: updated code to deal with the P128 corner case

--- idr0004-thorpe-rad52/generate_screens/make_screen.py
+++ idr0004-thorpe-rad52/generate_screens/make_screen.py
@@ -16,17 +16,27 @@ def parse_cl(argv):
     parser.add_argument("dir", metavar="DIR", help="dir")
     parser.add_argument("-o", "--output", metavar="FILE", help="output file")
     parser.add_argument("-p", "--plate", metavar="PLATE", help="plate name")
+    parser.add_argument("--rev-well-tag", action="store_true",
+                        help="well tags are reversed (e.g., 2b instead of b2")
     return parser.parse_args(argv[1:])
 
 
-def write_screen(data_dir, plate, outf):
+def first_existing(d, basenames):
+    for bn in basenames:
+        fn = os.path.join(d, bn)
+        if os.path.exists(fn):
+            return fn
+
+
+def write_screen(data_dir, plate, outf, rev_well_tag=False):
     writer = ScreenWriter(plate, ROWS, COLUMNS, FIELDS)
     for idx in xrange(ROWS * COLUMNS):
         r, c = writer.coordinates(idx)
-        well_tag = "%s%d" % (r.lower(), c)
-        fn = os.path.join(data_dir, well_tag)
-        if not os.path.isfile(fn):
-            sys.stderr.write("missing: %s\n" % fn)
+        well_tag = "%d%s" % (c, r) if rev_well_tag else "%s%d" % (r, c)
+        fn = first_existing(data_dir, [well_tag.lower(), well_tag])
+        if fn is None:
+            sys.stderr.write("missing: %s\n" %
+                             os.path.join(data_dir, well_tag.lower()))
             field_values = None
         else:
             field_values = [fn]
@@ -40,7 +50,7 @@ def main(argv):
         outf = open(args.output, "w")
     else:
         outf = sys.stdout
-    write_screen(args.dir, args.plate, outf)
+    write_screen(args.dir, args.plate, outf, rev_well_tag=args.rev_well_tag)
     if outf is not sys.stdout:
         outf.close()
 
