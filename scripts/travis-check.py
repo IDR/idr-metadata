c06e1e145c4bb30b4f8710060472d9ab660bc334
Simon Li <spli@dundee.ac.uk>
2015-10-07 21:55:00 +0200

    Add a hack to check columns aren't converted to indices

--- scripts/travis-check.py
+++ scripts/travis-check.py
@@ -24,9 +24,16 @@ for root, dirs, files in os.walk(sys.argv[1]):
 
         if fn.lower().endswith('.csv'):
             print fn
+            # pandas is way too tolerant of extra columns- they may be treated
+            # as a single or multi row index, or discarded. Since we don't
+            # use index columns the following seems to be the best way of
+            # checking none of the columns have been converted to an index.
+            # Unfortunately error messages may not be very helpful.
+            # This will also check for duplicate columns, `mangle_dupe_cols`
+            # means the column names will be different.
+            df_default = pandas.read_csv(fn)
             df = pandas.read_csv(fn, index_col=False, mangle_dupe_cols=False)
-            assert len(set(df.columns)) == len(df.columns), \
-                'Duplicate columns found'
+            pandas.util.testing.assert_frame_equal(df_default, df)
             for c in df.columns:
                 assert not c.startswith('Unnamed:'), \
                     'Unnamed column found'
