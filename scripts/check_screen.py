b72999af6117e674aafa5cdc5d6ec4599e0135ae
simleo <simleo@crs4.it>
2015-12-09 09:42:35 +0000

    check_screen: use ScreenReader

--- scripts/check_screen.py
+++ scripts/check_screen.py
@@ -2,10 +2,10 @@
 
 import sys
 import os
-from ConfigParser import ConfigParser
 from argparse import ArgumentParser
 
 from pyidr.file_pattern import FilePattern
+from pyidr.screenio import ScreenReader
 
 
 def parse_cl(argv):
@@ -16,21 +16,14 @@ def parse_cl(argv):
 
 def main(argv):
     args = parse_cl(argv)
-    cp = ConfigParser()
-    cp.optionxform = str
     with open(args.screen) as f:
-        cp.readfp(f)
-    for sec in cp.sections():
-        if not (sec.startswith("Well")):
-            continue
-        for k in cp.options(sec):
-            if not k.startswith("Field_"):
-                continue
-            pattern = cp.get(sec, k)
+        reader = ScreenReader(f)
+    for i, w in enumerate(reader.wells):
+        for j, pattern in enumerate(w["Fields"]):
             for fn in FilePattern(pattern).filenames():
                 if os.path.exists(fn):
                     continue
-                sys.stderr.write("ERROR[%s|%s]: missing %r\n" % (sec, k, fn))
+                sys.stderr.write("ERROR[%d|%d]: missing %r\n" % (i, j, fn))
 
 
 if __name__ == "__main__":
