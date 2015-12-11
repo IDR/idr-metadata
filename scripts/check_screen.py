10b9a541ecbb4cbe3f39788a555bb8a3760eb050
simleo <simleo@crs4.it>
2015-12-11 11:18:24 +0000

    check_screen: by default, only check for well-formedness

--- scripts/check_screen.py
+++ scripts/check_screen.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""\
+Check that the input file is a well-formed .screen file.  With '-e',
+also check that individual files from patterns in Field_* entries
+exist on disk.
+
+If everything is OK, there is no output and the return value is zero.
+Otherwise, the return value is nonzero and you should see something on
+stderr.
+"""
+
 import sys
 import os
 from argparse import ArgumentParser
@@ -9,22 +19,38 @@ from pyidr.screenio import ScreenReader
 
 
 def parse_cl(argv):
-    parser = ArgumentParser()
+    parser = ArgumentParser(description=__doc__)
     parser.add_argument("screen", metavar="SCREEN_FILE", help="screen file")
+    parser.add_argument("-e", "--check-existence", action="store_true",
+                        help="check existence of individual files in fields")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="verbose mode")
     return parser.parse_args(argv[1:])
 
 
-def main(argv):
-    args = parse_cl(argv)
-    with open(args.screen) as f:
-        reader = ScreenReader(f)
+def check_existence(reader):
+    retval = 0
     for i, w in enumerate(reader.wells):
         for j, pattern in enumerate(w["Fields"]):
             for fn in FilePattern(pattern).filenames():
                 if os.path.exists(fn):
                     continue
+                retval = 1
                 sys.stderr.write("ERROR[%d|%d]: missing %r\n" % (i, j, fn))
+    return retval
+
+
+def main(argv):
+    args = parse_cl(argv)
+    if args.verbose:
+        print "checking %r" % (args.screen,)
+    with open(args.screen) as f:
+        reader = ScreenReader(f)
+    # ScreenReader raises an exception if the file is not well-formed
+    if args.check_existence:
+        return check_existence(reader)
+    return 0
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
