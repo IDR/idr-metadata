a8c61234b377d576442d023d5f79fb9d55d65d31
Simon Li <spli@dundee.ac.uk>
2016-04-01 18:01:08 +0100

    Add script for setting rendering defs

---
+++ scripts/mini/idr-setup-4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Rendering settings
+
+date
+OMERO_SERVER=/home/omero/OMERO.server
+omero="$OMERO_SERVER/bin/omero"
+
+pushd idr-metadata
+
+# Don't forget hql -q outputs the row number as the first column
+$omero login -s localhost -u demo -w ome
+$omero hql 'select id,name from Screen' --limit -1 -q --style plain | \
+    while IFS='' read -r line; do
+        IFS=, read -a arr <<< "$line"
+        echo "id:${arr[1]} name:${arr[2]}"
+
+        # Get the first image in each screen
+        IFS=, read -a iarr <<<$( $omero hql -q --limit 1 --style plain "\
+            SELECT spl.parent.id,spl.parent.name,ws.image.id \
+            FROM WellSample ws, Plate p, ScreenPlateLink spl \
+            WHERE spl.child=p AND ws.well.plate=p \
+            AND spl.parent.id=${arr[1]}" )
+
+        stat ${arr[2]}/idr*-renderdef.txt >/dev/null 2> /dev/null|| continue
+        $omero render edit Image:${iarr[3]} $(cat ${arr[2]}/idr*-renderdef.txt)
+        $omero render copy Image:${iarr[3]} Screen:${arr[1]}
+    done
+
+popd
+date
+echo DONE
