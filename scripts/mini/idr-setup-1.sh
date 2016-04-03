c98681c8a6d4ecf6809d085a152d8a47b4e01f0c
Simon Li <orpheus+devel@gmail.com>
2016-04-03 18:20:40 +0100

    Check /uod/idr is mounted

--- scripts/mini/idr-setup-1.sh
+++ scripts/mini/idr-setup-1.sh
@@ -6,6 +6,11 @@ SMB_SHARE1=//orca-5.openmicroscopy.org/idr
 sudo mkdir -p /uod/idr
 sudo mount -t cifs -o username="$SMB_USER" "$SMB_SHARE1" /uod/idr
 
+if [ ! -d /uod/idr/filesets ]; then
+    echo "ERROR: IDR filesets not found, exiting"
+    exit 2
+fi
+
 # Optionally disable autostart of OMERO.server
 # because /uod/idr needs to be manually mounted
 sudo systemctl disable omero
