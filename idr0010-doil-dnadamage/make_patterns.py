1a57965a5d13a53e8ddabeee182f3f8b8cf50acf
jmoore <josh@glencoesoftware.com>
2016-05-02 14:25:44 +0000

    idr0010: fix DNA patterns
    There is a large diversity of naming conventions
    for the stk files including extra whitespace and
    typos. The make_patterns.py script did its best
    to take all of them into account.

---
+++ idr0010-doil-dnadamage/make_patterns.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from pyidr import file_pattern as fp
+from glob import glob
+from os.path import basename
+
+base = "/uod/idr/filesets/idr0010-doil-dnadamage/20150501-original/Restored GW screen/"
+plates = "idr-metadata/idr0010-doil-dnadamage/screenA/plates/"
+dirs = glob(base + "/*")
+
+def iglob(pattern):
+  def either(c):
+    return '[%s%s]'%(c.lower(),c.upper()) if c.isalpha() else c
+  return glob(''.join(map(either,pattern)))
+
+
+for dir in dirs:
+  try:
+    dapi, = [x for x in iglob(dir + "/*dapi*") if "spot" not in x]
+    try:
+      bp, = [x for x in iglob(dir + "/*bp*") if "spot" not in x]
+    except: # Try BB
+      bp, = [x for x in iglob(dir + "/*bb*") if "spot" not in x]
+  except:
+    raise Exception(dir)
+
+  plate = basename(dir)  
+  bp = bp.replace("bp", "bxxx")
+  rv = fp.find_pattern_2seq(dapi, bp)
+  rv = rv.replace("bxxx", "bp")
+  print plate, " ".join(rv.split("/")[-1].split(" ")[1:])
+  with open("%s/%s" % (plates, plate), "w") as screen_file:
+      screen_file.write("%s/%s.pattern\n" % (plates, plate))
+  with open("%s/%s.pattern" % (plates, plate), "w") as pattern_file:
+      pattern_file.write(rv)
