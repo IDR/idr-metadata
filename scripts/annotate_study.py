76ca3af51a8676401f73cd5fe750c83b075661b6
Simon Li <spli@dundee.ac.uk>
2016-10-11 16:37:25 +0100

    Switch to csv module. Add more top-level study keys.

--- scripts/annotate_study.py
+++ scripts/annotate_study.py
@@ -2,6 +2,7 @@
 
 """
 Create a map-annotation on a top-level object based on an IDR study file.
+All fields are treated as optional
 Example:
 
     scripts/annotate_study.py \
@@ -9,7 +10,7 @@ Example:
 """
 
 import argparse
-import pandas as pd
+import csv
 
 from pyidr.mapannotations import create_map_annotation
 # This import needs to come after the above, otherwise there's a mysterious
@@ -20,38 +21,38 @@ from omero.cli import CLI, ProxyStringType
 # Mapping of
 # - study keys (from the study file)
 # - displayed names
-# - displayed values
+# - displayed values (default value)
 study_keys = [
     ('Study PubMed ID', 'PubMed ID'),
-    ('Study PubMed ID', 'PubMed URL',
+    ('Study PubMed ID', 'PubMed ID URL',
      'http://www.ncbi.nlm.nih.gov/pubmed/{{ value }}'),
     ('Study DOI', 'Publication DOI'),
+    ('Study DOI', 'Publication DOI URL', 'http://dx.doi.org/{{ value }}'),
+    ('Study Publication Title', 'Title'),
+    ('Study Author List', 'Authors'),
+    ('Study External URL', 'External URL'),
 ]
 
 ns = 'openmicroscopy.org/idr/study'
 
 
 def get_pairs(study):
-    # The study files are ragged tables, so Pandas will fill in additional
-    # cells with NaN.
-    df = pd.read_table(study, sep='\t', header=None)
+    table = csv.reader(study, delimiter='\t')
 
     kvpairs = []
-    for items in study_keys:
-        key = items[0]
-        display = items[1]
-        try:
-            template = items[2]
-        except IndexError:
-            template = '{{ value }}'
+    for row in table:
+        for items in study_keys:
+            key = items[0]
+            display = items[1]
+            try:
+                template = items[2]
+            except IndexError:
+                template = '{{ value }}'
 
-        rows = df.loc[df[0] == key]
-        # TODO: Allow missing?
-        if len(rows) != 1:
-            raise Exception(
-                'Expected 1 row matching %s, found %d' % (key, len(rows)))
-        value = template.replace('{{ value }}', rows.iloc[0, 1])
-        kvpairs.append((display, value))
+            # All keys are optional, duplicates allowed, omit empty value
+            if len(row) > 1 and row[0] == key:
+                value = template.replace('{{ value }}', row[1])
+                kvpairs.append((display, value))
 
     return kvpairs
 
@@ -71,7 +72,8 @@ def run(studyfile, targetstr):
         us = session.getUpdateService()
 
         target = ProxyStringType()(targetstr)
-        rowkvs = get_pairs(studyfile)
+        with open(studyfile) as f:
+            rowkvs = get_pairs(f)
         links = create_map_annotation([target], rowkvs, ns)
         ids = us.saveAndReturnIds(links)
         print 'Created MapAnnotation links: %s' % ids
@@ -80,13 +82,14 @@ def run(studyfile, targetstr):
             cli.close()
 
 
-def main():
+def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('studyfile')
     parser.add_argument('target')
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     run(args.studyfile, args.target)
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+    main(sys.argv[1:])
