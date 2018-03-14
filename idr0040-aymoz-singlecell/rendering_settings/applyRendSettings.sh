dfe1eabca41c763b71d88d98d3702791be425240
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-03-14 11:05:05 +0000

    Fixed issue with rendering settings mappings

--- idr0040-aymoz-singlecell/rendering_settings/applyRendSettings.sh
+++ idr0040-aymoz-singlecell/rendering_settings/applyRendSettings.sh
@@ -4,7 +4,7 @@ omero="/opt/omero/server/OMERO.server/bin/omero"
 
 while IFS='	' read -r f1 f2
 do
-	imageids=`$omero hql --ids-only --style csv -q "select img from DatasetImageLink l join l.parent as ds join l.child as img where ds.name = '$f2'"`
+	imageids=`$omero hql --ids-only --limit 100 --style csv -q "select img from DatasetImageLink l join l.parent as ds join l.child as img where ds.name = '$f1'"`
 	IFS=',' read -r -a array <<< $imageids
 	
 	for imageid in "${array[@]}"
@@ -12,8 +12,8 @@ do
 		imageid=${imageid/ */}
 		if [[ $imageid == Image* ]]
 		then
-    			printf 'Applying rendering settings %s to %s \n' "$f1" "$imageid"
-			$omero render edit $imageid $f1
+			printf 'Applying rendering settings %s (dataset %s) to %s \n' "$f2" "$f1" "$imageid"
+			$omero render edit $imageid $f2
 		fi
 	done
 done <"$file"
