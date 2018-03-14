2382e418542f762b457a1a19b8b7d9a792aefe89
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-03-14 14:44:00 +0000

    Some tidying up

--- scripts/rendering/applyRendSettings.sh
+++ scripts/rendering/applyRendSettings.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# The render plugin currently doesn't support applying a set of rendering settings to a 
+# whole dataset. This script just itereates over a tab separated mapping file which
+# maps a dataset name to a renderings settings file (can be json or yml).
+# It retrieves the images ids of the datasets and calls the render plugin for each
+# of the images. 
+#
+# Note: 
+# - You need to log in first before running this script.
+# - If any of the datasets has more than 100 images increase the --limit parameter!
+
 file="mapping.tsv"
 omero="/opt/omero/server/OMERO.server/bin/omero"
 
